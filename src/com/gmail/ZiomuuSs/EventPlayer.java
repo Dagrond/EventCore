b1d955db638c5a7a6b11e225f4f025db6e852ff5
Dagrond <ZiomuuSs@gmail.com>
2018-10-25 18:36:29 +0200

    NOPE NOPE NOPE

--- src/com/gmail/ZiomuuSs/EventPlayer.java
+++ src/com/gmail/ZiomuuSs/EventPlayer.java
@@ -1,84 +1,28 @@
 package com.gmail.ZiomuuSs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-
-import com.gmail.ZiomuuSs.Utils.Data;
-import com.gmail.ZiomuuSs.Utils.Msg;
-
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
 
 public class EventPlayer {
-  protected UUID uuid; //player's uuid
-  protected Inventory inv; //player's inventory
-  protected Location loc; //player's location
-  protected int lvl; //player's level
-  protected Data data;
+	private UUID uuid;
+  private Location loc;
+  private double Exp;
+  private double Health;
+  private double MaxHealth;
+  private float Saturation;
+  private int Hunger;
+  private GameMode Gamemode;
+  private boolean Fly;
+  private ArrayList<PotionEffect> potions = new ArrayList<PotionEffect>();
+  private HashMap<Integer, ItemStack> items = new HashMap<Integer, ItemStack>();
   
-  public EventPlayer(UUID uuid, Inventory inv, Location loc, int lvl, Location lobby, Data data) {
-    this.uuid = uuid;
-    this.inv = inv;
-    this.loc = loc;
-    this.lvl = lvl;
-    this.data = data;
-    Player player = Bukkit.getPlayer(uuid);
-    if (player != null) {
-      player.getInventory().clear();
-      player.setLevel(0);
-      player.setTotalExperience(0);
-      player.teleport(lobby);
-    }
-  }
-
-  public boolean startEvent(Inventory i, Location l) {
-    Player player = Bukkit.getPlayer(uuid);
-    if (player != null) {
-      player.getInventory().clear();
-      player.getInventory().setContents(i.getContents());
-      player.teleport(l);
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  public boolean quit() {
-    Player player = Bukkit.getPlayer(uuid);
-    if (player != null) {
-      player.getInventory().clear();
-      player.getInventory().setContents(inv.getContents());
-      player.setLevel(lvl);
-      player.teleport(loc);
-      player.sendMessage(Msg.get("error_event_lose", true));
-      data.broadcastToEventPlayers(Msg.get("player_leave", true, player.getDisplayName(), Integer.toString(data.getPlayerAmount())));
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  public boolean won(String name) {
-    Player player = Bukkit.getPlayer(uuid);
-    if (player != null) {
-      player.getInventory().clear();
-      player.getInventory().setContents(inv.getContents());
-      player.setLevel(lvl);
-      player.teleport(loc);
-      player.sendMessage(Msg.get("event_won", true));
-      data.getEvent(name).getReward().grant(player);
-      Bukkit.broadcastMessage(Msg.get("event_end", true, player.getDisplayName(), name));
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  @Override
-  public String toString() {
-    return uuid.toString();
-    //ironic...
+  public EventPlayer() {
+  	
   }
 }
--- src/com/gmail/ZiomuuSs/Events/Event.java
+++ src/com/gmail/ZiomuuSs/Events/Event.java
@@ -5,158 +5,27 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 
 import com.gmail.ZiomuuSs.EventPlayer;
 import com.gmail.ZiomuuSs.Main;
-import com.gmail.ZiomuuSs.Reward;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class Event implements Listener {
   public static enum EventStatus {
-    NOT_READY, READY, IN_PROGRESS, IN_LOBBY;
-  }
-  public static enum EventMode {
-    SPLEEF;
-    public static boolean contains(String s) {
-      for (EventMode c : EventMode.values()) {
-          if (c.name().equals(s.toUpperCase())) {
-              return true;
-          }
-      }
-      return false;
-  }
-    public static String getString() {
-      String x = "";
-      for (EventMode e : EventMode.values()) {
-        x += e.toString()+", ";
-      }
-      return x.substring(0, x.length() - 2);
-    }
-  }
-  public static enum REQUIMENT {
-    LOBBY, STARTPOINTS, SURFACE, MINY, MINPLAYERS, MAXPLAYERS, INVENTORY;
+    NOT_READY, READY, IN_PROGRESS, STARTING;
   }
   protected Main plugin;
-  EventMode mode;
   protected String name; //name of event
   protected boolean startWhenMax; //Should event start (ignoring delay) when amount of players in lobby hit max?
   protected HashMap<UUID, EventPlayer> players = new HashMap<>(); //Players in event
-  protected Location lobby; //coordinates of lobby
   protected ArrayList<Location> startPoints = new ArrayList<>(); //coordinates of spawn points when event begin
   protected Inventory startInventory; //Inventory for every player when event begins
   protected int maxPlayers = -1; //max players in event
-  protected Reward reward; //reward for winner(s)
   protected int minPlayers = 2; //min amount of players to start event
   protected EventStatus status; //Status of event (lobby(players are in lobby, event will start soon)/during(event is in progress)/off(event disabled to use or not ready to use)/free(there is not event))
   protected int delay; //amount of time in seconds after event will start (change it's status from "lobby" to "during"). Can be bypassed by command or if StartWhenMax=true
   
-  public Event (String name, Main plugin) {
-    this.name = name;
-    this.plugin = plugin;
-    plugin.getServer().getPluginManager().registerEvents(this, plugin);
-    status = EventStatus.NOT_READY;
-    startWhenMax = true;
-    minPlayers = 2;
-    delay = 60;
-  }
+  public Event () {}
   
-  public void kickPlayer (UUID uuid) {
-    players.get(uuid).quit();
-    players.remove(uuid);
-  }
-  
-  public void showRequiments(CommandSender sender) {}
-  
-  // Getters & Setters
-  public HashMap<UUID, EventPlayer> getPlayers() {
-    return players;
-  }
-  
-  //returns false if an start point was added, or returns true if an start point was edited
-  public boolean setStartPoints(Location loc, int index) {
-    if (index > 0) {
-      --index; //player counts from 1, Java from 0
-      if (startPoints.size()>= index+1) {
-        startPoints.set(index, loc);
-        return true;
-      } else {
-        startPoints.add(loc);
-        return false;
-      }
-    } else {
-      startPoints.add(loc);
-      return false;
-    }
-  }
-  
-  public void start() {}
-  public void setSurface(ProtectedRegion rg, World world) {}
-  public void setSurfaceMaterial(Material m) {}
-  public void setStartInventory(Inventory inv) {
-    startInventory = inv;
-  }
-  public Inventory getStartInventory() {
-    return startInventory;
-  }
-  public boolean isRequired (REQUIMENT r) {
-    return false;
-  }
-  public Reward getReward() {
-    return reward;
-  }
-  public void setMinY(int miny) {}
-  public void setMaxPlayers(int mp) {
-    maxPlayers = mp;
-  }
-  public void setMinPlayers(int mp) {
-    minPlayers = mp;
-  }
-  public int getMinPlayes() {
-    return minPlayers;
-  }
-  @Override
-  public String toString() {
-    return name;
-  }
-  public EventMode getMode() {
-    return mode;
-  }
-  public EventStatus getStatus() {
-    return status;
-  }
-  public int getMaxPlayers() {
-    return maxPlayers;
-  }
-  public int getMinPlayers() {
-    return minPlayers;
-  }
-  public int getDelay() {
-    return delay;
-  }
-  public Inventory getStartingInventory() {
-    return startInventory;
-  }
-  public ArrayList<Location> getStartPoints() {
-    return startPoints;
-  }
-  public Location getLobby() {
-    return lobby;
-  }
-  public void setLobby(Location l) {
-    lobby = l;
-  }
-  public boolean isInEvent(UUID uuid) {
-    if (players.containsKey(uuid))
-      return true;
-    else
-      return false;
-  }
-  public boolean isStartingWhenMax() {
-    return startWhenMax;
-  }
 }
